d567580504404f339affd047dace6e17aa49cbd5
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-19 23:15:16 +0530

    sidebar done

--- src/templates/Sidebar.jsx
+++ src/templates/Sidebar.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { RiMovie2AiFill } from "react-icons/ri";
+import { MdTheaterComedy } from "react-icons/md";
+import { GiTriceratopsHead } from "react-icons/gi";
+import { BiSlideshow } from "react-icons/bi";
+import { MdContactEmergency } from "react-icons/md";
+import { AiFillInfoCircle } from "react-icons/ai";
 
 const Sidebar = () => {
     return (
@@ -11,10 +17,16 @@ const Sidebar = () => {
             <nav className='flex flex-col gap-3'>
                 <h1 className='mt-10 mb-5 text-white text-xl font-semibold'>New Feeds</h1>
                 <Link className='text-white/70 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg flex gap-3 items-center'><i class="ri-fire-fill text-xl"></i> Trending</Link>
-                <Link className='text-white/70 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg'>Popular</Link>
-                <Link className='text-white/70 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg'>Movies</Link>
-                <Link className='text-white/70 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg'>Tv Shows</Link>
-                <Link className='text-white/70 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg'>People</Link>
+                <Link className='text-white/70 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg flex gap-3 items-center'><GiTriceratopsHead className='text-xl'></GiTriceratopsHead> Popular</Link>
+                <Link className='text-white/70 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg flex gap-3 items-center'><RiMovie2AiFill className='text-xl'></RiMovie2AiFill> Movies</Link>
+                <Link className='text-white/70 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg flex gap-3 items-center'><BiSlideshow className='text-xl'></BiSlideshow> Tv Shows</Link>
+                <Link className='text-white/70 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg flex gap-3 items-center'><MdTheaterComedy className='text-xl'></MdTheaterComedy> People</Link>
+            </nav>
+            <hr className='text-white/30 h-[1px]' />
+            <nav className='flex flex-col gap-3 '>
+                <Link className='text-white/70 mt-7 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg flex gap-3 items-center'><AiFillInfoCircle></AiFillInfoCircle> About</Link>
+                <Link className='text-white/70 text-xl p-5 duration-150 hover:text-white  hover:bg-[#6556CD] rounded-lg flex gap-3 items-center'><MdContactEmergency className='text-xl'></MdContactEmergency> Contact Us</Link>
+
             </nav>
         </div>
     )
