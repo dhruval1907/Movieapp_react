440864c10986872c12af8bac794a8a5434daa101
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-20 11:12:21 +0530

    done

--- src/templates/Navbar.jsx
+++ src/templates/Navbar.jsx
@@ -1,40 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaSearch } from "react-icons/fa";
 import { RxCross2 } from "react-icons/rx";
 import { Link } from 'react-router-dom';
+import { CiSearch } from "react-icons/ci";
 
 const Navbar = () => {
+
+  const [query, setquery] = useState("")
+
+  console.log(query);
+
+
   return (
     <div className=' p-6 w-full relative'>
       <nav className='gap-5 w-full flex  items-center justify-center'>
-        <FaSearch className='text-white/70 text-2xl' />
-        <input type="text" className='p-5 w-[55%] border-none outline-none text-xl text-white/40 font-semibold ' />
-        <RxCross2 className='text-white/70  text-2xl' />
-        <div className='w-[50%] h-[25rem]  bg-zinc-200  absolute top-[90%] overflow-auto '>
-          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
+        <CiSearch className='text-white/70 text-xl' />
+        <input type="text" className='p-5 w-[55%] border-none outline-none text-xl text-white/90 font-extrabold
+          'value={query}
+          placeholder='search anything'
+          onChange={(e) => setquery(e.target.value)}
+        />
+        {query.length > 0 && <RxCross2 className=  'text-white/70 cursor-pointer  text-2xl ' onClick={()=>setquery("")} />}
+        <div className='w-[50%] max-h-[25rem]  bg-zinc-200  absolute top-[90%] overflow-auto '> 
+          {/* <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-black/20 hover:text-black hover:bg-zinc-300 duration-100'>
             <img src="" alt="" />
             <span>Hello Eveyone</span>
-          </Link>
-          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
-            <img src="" alt="" />
-            <span>Hello Eveyone</span>
-          </Link>
-          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
-            <img src="" alt="" />
-            <span>Hello Eveyone</span>
-          </Link>
-          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
-            <img src="" alt="" />
-            <span>Hello Eveyone</span>
-          </Link>
-          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
-            <img src="" alt="" />
-            <span>Hello Eveyone</span>
-          </Link>
-          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
-            <img src="" alt="" />
-            <span>Hello Eveyone</span>
-          </Link>
+          </Link> */}
         </div>
       </nav>
     </div>
