485075b86c5fcc565559d1fa75c63230ba357c4d
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-20 10:13:36 +0530

    chaning css

--- src/templates/Navbar.jsx
+++ src/templates/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaSearch } from "react-icons/fa";
 import { RxCross2 } from "react-icons/rx";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -8,9 +9,32 @@ const Navbar = () => {
       <nav className='gap-5 w-full flex  items-center justify-center'>
         <FaSearch className='text-white/70 text-2xl' />
         <input type="text" className='p-5 w-[55%] border-none outline-none text-xl text-white/40 font-semibold ' />
-        <RxCross2 className='text-white/70 text-2xl' />
-        <div className='w-[50%] h-[22rem] bg-red-300 absolute top-[90%] '>
-
+        <RxCross2 className='text-white/70  text-2xl' />
+        <div className='w-[50%] h-[25rem]  bg-zinc-200  absolute top-[90%] overflow-auto '>
+          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
+            <img src="" alt="" />
+            <span>Hello Eveyone</span>
+          </Link>
+          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
+            <img src="" alt="" />
+            <span>Hello Eveyone</span>
+          </Link>
+          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
+            <img src="" alt="" />
+            <span>Hello Eveyone</span>
+          </Link>
+          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
+            <img src="" alt="" />
+            <span>Hello Eveyone</span>
+          </Link>
+          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
+            <img src="" alt="" />
+            <span>Hello Eveyone</span>
+          </Link>
+          <Link className=' w-full flex border-b-2 border-zinc-100  items-start p-7  font-semibold text-white hover:text-black hover:bg-zinc-300 duration-100'>
+            <img src="" alt="" />
+            <span>Hello Eveyone</span>
+          </Link>
         </div>
       </nav>
     </div>
